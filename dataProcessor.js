2beb14acd3fa15a36ecc91bd7c03e53f9b9853e2
Sathish Prabhakar <Satish.Prabhakar@netapp.com>
2020-03-20 12:03:32 +0530

    complexity reduced

--- dataProcessor.js
+++ dataProcessor.js
@@ -48,41 +48,33 @@ const calculateSentimentScore = function calculateSentimentScore(usersList) {
   if (!usersList || usersList.length == 0) {
     return null;
   }
-
-  let participationCount = 0;
+  let participatedUsers = [];
   let sentimentScore = 0;
-
-  usersList.forEach(userId => {
-    const filtered = sampeData.VOTES_DATA.filter(
-      vote => vote.userId === userId
-    );
-    if (filtered && filtered.length > 0) {
-      participationCount++;
-
-      const sentiScoreForUser = filtered.reduce((acc, vote) => {
-        const voteVal = vote.Vote;
-        let sentimentValue = 0;
-
-        if (voteVal === 4 || voteVal === 5) {
-          sentimentValue = 1;
-        } else if (voteVal === 1 || voteVal === 2) {
-          sentimentValue = -1;
-        } else if (voteVal === 3) {
-          // nothing
-        } else {
-          // nothing
-        }
-
-        acc = acc + sentimentValue;
-        return acc;
-      }, 0);
-
-      sentimentScore = sentimentScore + sentiScoreForUser;
+  sampeData.VOTES_DATA.forEach(vote => {
+    const userId = vote.userId;
+    if (usersList.includes(userId)) {
+      if (!participatedUsers.includes(userId)) {
+        participatedUsers.push(userId);
+      }
+      const voteVal = vote.Vote;
+      if (voteVal === 4 || voteVal === 5) {
+        sentimentScore = sentimentScore + 1;
+      } else if (voteVal === 1 || voteVal === 2) {
+        sentimentScore = sentimentScore - 1;
+      } else if (voteVal === 3) {
+        // nothing
+      } else {
+        // nothing
+      }
     }
   });
-
-  const participationPercentage = (participationCount / usersList.length) * 100;
-
+  const participationCount = participatedUsers.length;
+  let participationPercentage = 0;
+  if (participationCount != 0) {
+    participationPercentage = Number(
+      ((participationCount / usersList.length) * 100).toFixed(2)
+    );
+  }
   return {
     sentimentScore,
     participationPercentage
